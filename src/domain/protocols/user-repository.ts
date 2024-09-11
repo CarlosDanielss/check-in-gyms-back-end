7c9275478176f8eb3b253a210c4460df662db53a
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-11 15:59:53 -0300

    feat: add filter by id in user repository protocol

--- src/domain/protocols/user-repository.ts
+++ src/domain/protocols/user-repository.ts
@@ -4,6 +4,7 @@ export type UserRepositoryCreate = Pick<User, "name" | "email" | "password"> &
   Partial<Pick<User, "role">>;
 
 export interface UserRepository {
+  findById(id: string): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
   create(user: UserRepositoryCreate): Promise<User>;
 }
