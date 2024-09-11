0621adb51a5c0372aea8cfdadf813c748c2ba84f
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-11 20:41:12 -0300

    feat: add update function in user validator adapter

--- src/infra/validations/user-validator-adapter.ts
+++ src/infra/validations/user-validator-adapter.ts
@@ -3,11 +3,49 @@ import { z } from "zod";
 import {
   UserValidator,
   UserValidatorRegister,
+  UserValidatorUpdate,
 } from "@/presentation/protocols/user-validator.js";
 
 import { InvalidCredentialsError } from "@/domain/erros/invalid-credentials-error.js";
 
 export class UserValidatorAdapter implements UserValidator {
+  update({ id, name, email }: UserValidatorUpdate): UserValidatorUpdate {
+    const updateSchema = z
+      .object({
+        id: z.string().uuid(),
+        name: z
+          .string()
+          .min(5)
+          .trim()
+          .toLowerCase()
+          .regex(/^[^\d]*$/, "The name must not contain any numbers.")
+          .optional(),
+        email: z.string().email().trim().optional(),
+      })
+      .refine(
+        (data) => {
+          const hasName = Boolean(data.name);
+          const hasEmail = Boolean(data.email);
+
+          return hasName ?? hasEmail;
+        },
+        {
+          message:
+            "If currentPassword or newPassword are provided, both must be provided.",
+        }
+      );
+
+    const isValid = updateSchema.safeParse({ id, name, email });
+
+    if (isValid.error) {
+      const { path, message } = isValid.error.issues[0];
+
+      throw new InvalidCredentialsError(String(path[0]), message);
+    }
+
+    return isValid.data;
+  }
+
   register({
     name,
     email,
