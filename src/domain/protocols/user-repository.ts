9c0b05a61942753973220261bd5a8b880ea69d59
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-11 20:42:12 -0300

    feat: add update function in user repository protocols

--- src/domain/protocols/user-repository.ts
+++ src/domain/protocols/user-repository.ts
@@ -1,9 +1,13 @@
 import { User } from "@/domain/models/user.js";
 
+export type UserRepositoryUpdate = Pick<User, "id"> &
+  Partial<Pick<User, "name" | "email">>;
+
 export type UserRepositoryCreate = Pick<User, "name" | "email" | "password"> &
   Partial<Pick<User, "role">>;
 
 export interface UserRepository {
+  update(user: UserRepositoryUpdate): Promise<User>;
   findById(id: string): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
   create(user: UserRepositoryCreate): Promise<User>;
