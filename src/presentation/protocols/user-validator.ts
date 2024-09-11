3699ce4af80be2b3d44b954d0c8ad36a06af9e24
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-11 20:39:51 -0300

    feat: add update function in user protocols

--- src/presentation/protocols/user-validator.ts
+++ src/presentation/protocols/user-validator.ts
@@ -1,7 +1,11 @@
 import { User } from "@/domain/models/user.js";
 
+export type UserValidatorUpdate = Pick<User, "id"> &
+  Partial<Pick<User, "name" | "email">>;
+
 export type UserValidatorRegister = Pick<User, "name" | "email" | "password">;
 
 export interface UserValidator {
+  update(user: UserValidatorUpdate): UserValidatorUpdate;
   register(user: UserValidatorRegister): UserValidatorRegister;
 }
