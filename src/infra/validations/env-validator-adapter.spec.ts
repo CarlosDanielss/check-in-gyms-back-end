76eb5c9c84d479e85634d5b08903de5c5e1dbb5f
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-13 16:18:45 -0300

    test: add test in env validator adapter

---
+++ src/infra/validations/env-validator-adapter.spec.ts
@@ -0,0 +1,90 @@
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { EnvValidatorAdapter } from "./env-validator-adapter.js";
+
+let sut: EnvValidatorAdapter;
+
+describe("Env Validator Adapter", () => {
+  beforeEach(() => {
+    sut = new EnvValidatorAdapter();
+  });
+
+  it("should return an error if the NODE_ENV field is not provided", () => {
+    const environment = {
+      JWT_SECRET: "jwtsecrettest",
+      PORT: "3333",
+      DATABASE_URL: "postgresql://test:123456@localhost:5432/test",
+    };
+
+    expect(() => sut.exec(environment)).toThrow(
+      Error(`Invalid environment variables [NODE_ENV]`)
+    );
+  });
+
+  it("should return an error if the JWT_SECRET field is not provided", () => {
+    const environment = {
+      NODE_ENV: "test",
+      PORT: "3333",
+      DATABASE_URL: "postgresql://test:123456@localhost:5432/test",
+    };
+
+    expect(() => sut.exec(environment)).toThrow(
+      Error(`Invalid environment variables [JWT_SECRET]`)
+    );
+  });
+
+  it("should set the PORT to [3333] if the PORT field is not provided", () => {
+    const environment = {
+      NODE_ENV: "test",
+      JWT_SECRET: "jwtsecrettest",
+      DATABASE_URL: "postgresql://test:123456@localhost:5432/test",
+    };
+
+    const validation = sut.exec(environment);
+
+    expect(validation).toEqual({
+      NODE_ENV: "test",
+      JWT_SECRET: "jwtsecrettest",
+      PORT: 3333,
+      DATABASE_URL: "postgresql://test:123456@localhost:5432/test",
+    });
+  });
+
+  it("should return an error if the DATABASE_URL field is not provided", () => {
+    const environment = {
+      NODE_ENV: "test",
+      JWT_SECRET: "jwtsecrettest",
+      PORT: "3333",
+    };
+
+    expect(() => sut.exec(environment)).toThrow(
+      Error(`Invalid environment variables [DATABASE_URL]`)
+    );
+  });
+
+  it("should return an error if the NODE_ENV field receives a value other than developer, test or production", () => {
+    const environment = {
+      NODE_ENV: "rain",
+      JWT_SECRET: "jwtsecrettest",
+      PORT: "3333",
+      DATABASE_URL: "postgresql://test:123456@localhost:5432/test",
+    };
+
+    expect(() => sut.exec(environment)).toThrow(
+      Error(`Invalid environment variables [NODE_ENV]`)
+    );
+  });
+
+  it("should return an error if the DATABASE_URL field does not have a valid link format", () => {
+    const environment = {
+      NODE_ENV: "test",
+      JWT_SECRET: "jwtsecrettest",
+      PORT: "3333",
+      DATABASE_URL: "testdatabaseurl",
+    };
+
+    expect(() => sut.exec(environment)).toThrow(
+      Error(`Invalid environment variables [DATABASE_URL]`)
+    );
+  });
+});
--- src/infra/validations/env-validator-adapter.ts
+++ src/infra/validations/env-validator-adapter.ts
@@ -5,22 +5,22 @@ import { env, EnvValidator } from "@/main/protocols/env-validator.js";
 export class EnvValidatorAdapter implements EnvValidator {
   exec(environment: NodeJS.ProcessEnv): env {
     const envSchema = z.object({
-      NODE_ENV: z
-        .enum(["developer", "test", "production"])
-        .default("developer"),
+      NODE_ENV: z.enum(["developer", "test", "production"]),
       JWT_SECRET: z.string(),
       PORT: z.coerce.number().default(3333),
-      DATABASE_URL: z.string(),
+      DATABASE_URL: z.string().url(),
     });
 
-    const isValid = envSchema.safeParse(environment);
+    const validation = envSchema.safeParse(environment);
 
-    if (!isValid.success) {
-      console.error("Invalid environment variables", isValid.error.format());
+    if (validation.error) {
+      const { path } = validation.error.issues[0];
+      const fieldWithError = `Invalid environment variables [${path}]`;
 
-      throw new Error("Invalid environment variables.");
+      console.error("Invalid environment variables", validation.error.format());
+      throw new Error(fieldWithError);
     }
 
-    return isValid.data;
+    return validation.data;
   }
 }
