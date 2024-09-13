0f7f390459383af6235ce225c12d47f9dcbd1bd2
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-13 11:17:06 -0300

    feat: add user validator read in protocols

--- src/presentation/protocols/user-validator.ts
+++ src/presentation/protocols/user-validator.ts
@@ -1,11 +1,14 @@
 import { User } from "@/domain/models/user.js";
 
+export type UserValidatorRead = { page: number };
+
 export type UserValidatorUpdate = Pick<User, "id"> &
   Partial<Pick<User, "name" | "email">>;
 
 export type UserValidatorRegister = Pick<User, "name" | "email" | "password">;
 
 export interface UserValidator {
+  read(page: string): UserValidatorRead;
   update(user: UserValidatorUpdate): UserValidatorUpdate;
   register(user: UserValidatorRegister): UserValidatorRegister;
 }
