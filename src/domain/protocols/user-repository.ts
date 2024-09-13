a463ae39d6713695f7cc4c53cc3a8d3e2d5adb9d
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-13 11:18:03 -0300

    feat: add search many function in user repository protocols

--- src/domain/protocols/user-repository.ts
+++ src/domain/protocols/user-repository.ts
@@ -1,5 +1,9 @@
 import { User } from "@/domain/models/user.js";
 
+export type UserRepositorySearchMany = Partial<
+  Pick<User, "name" | "email" | "role">
+>;
+
 export type UserRepositoryUpdate = Pick<User, "id"> &
   Partial<Pick<User, "name" | "email">>;
 
@@ -7,6 +11,7 @@ export type UserRepositoryCreate = Pick<User, "name" | "email" | "password"> &
   Partial<Pick<User, "role">>;
 
 export interface UserRepository {
+  searchMany(page: number, query?: UserRepositorySearchMany): Promise<User[]>;
   update(user: UserRepositoryUpdate): Promise<User>;
   findById(id: string): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
