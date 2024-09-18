bcfc929bc39fc11470ca548844477f74729594c2
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-18 15:52:37 -0300

    refactor: add cpnj field in gym model

--- src/domain/models/gym.ts
+++ src/domain/models/gym.ts
@@ -3,6 +3,7 @@ import { CheckIn } from "./check-in.js";
 export type Gym = {
   id: string;
   title: string;
+  cnpj: string;
   description?: string | null;
   phone?: string | null;
   latitude: number | string;
