a656b403b576d3f758a54c97c0b232497601cbe0
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-09-25 15:26:04 -0300

    feat: add route search in gym

--- src/main/routes/gym-routes.ts
+++ src/main/routes/gym-routes.ts
@@ -4,6 +4,7 @@ import { routeAdapter } from "../config/route-adapter.js";
 
 import { CreateControllerFactory } from "@/presentation/controllers/gym/create-factory.js";
 import { NearbyGymControllerFactory } from "@/presentation/controllers/gym/nearby-gym-factory.js";
+import { SearchControllerFactory } from "@/presentation/controllers/gym/search-factory.js";
 
 export const gymsRoutes: FastifyPluginCallback = (
   app: FastifyInstance,
@@ -12,6 +13,7 @@ export const gymsRoutes: FastifyPluginCallback = (
 ) => {
   app.post("/", routeAdapter(CreateControllerFactory()));
   app.post("/nearby", routeAdapter(NearbyGymControllerFactory()));
+  app.post("/search", routeAdapter(SearchControllerFactory()));
 
   done();
 };
